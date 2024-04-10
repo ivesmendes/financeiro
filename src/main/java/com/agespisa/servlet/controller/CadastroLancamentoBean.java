14860560991ff02ae4a1b6908431a3bb8d798b9e
ivesmendes <ivesmedes@gmail.com>
2024-04-10 12:16:33 -0300

    Método adicionar() em Lancamentos, novo repositório Pessoas com métodos de consulta, nova classe controller CadastroLancamentoBean, novo conversor, e nova interface gráfica

---
+++ src/main/java/com/agespisa/servlet/controller/CadastroLancamentoBean.java
@@ -0,0 +1,75 @@
+package com.agespisa.servlet.controller;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+import com.agespisa.servlet.model.Lancamento;
+import com.agespisa.servlet.model.Pessoa;
+import com.agespisa.servlet.model.TipoLancamento;
+import com.agespisa.servlet.repository.Lancamentos;
+import com.agespisa.servlet.repository.Pessoas;
+import com.agespisa.servlet.service.CadastroLancamentos;
+import com.agespisa.servlet.service.NegocioException;
+import com.agespisa.servlet.util.JpaUtil;
+
+@ManagedBean
+@ViewScoped
+public class CadastroLancamentoBean implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private Lancamento lancamento = new Lancamento();
+	private List<Pessoa> todasPessoas;
+
+	public void prepararCadastro() {
+		EntityManager manager = JpaUtil.getEntityManager();
+		try {
+			Pessoas pessoas = new Pessoas(manager);
+			this.todasPessoas = pessoas.todas();
+		} finally {
+			manager.close();
+		}
+	}
+
+	public void salvar() {
+		EntityManager manager = JpaUtil.getEntityManager();
+		EntityTransaction trx = manager.getTransaction();
+		FacesContext context = FacesContext.getCurrentInstance();
+		try {
+			trx.begin();
+			CadastroLancamentos cadastro = new CadastroLancamentos(new Lancamentos(manager));
+			cadastro.salvar(this.lancamento);
+			this.lancamento = new Lancamento();
+			context.addMessage(null, new FacesMessage("Lançamento salvo com sucesso!"));
+			trx.commit();
+		} catch (NegocioException e) {
+			trx.rollback();
+			FacesMessage mensagem = new FacesMessage(e.getMessage());
+			mensagem.setSeverity(FacesMessage.SEVERITY_ERROR);
+			context.addMessage(null, mensagem);
+		} finally {
+			manager.close();
+		}
+	}
+
+	public List<Pessoa> getTodasPessoas() {
+		return this.todasPessoas;
+	}
+
+	public TipoLancamento[] getTiposLancamentos() {
+		return TipoLancamento.values();
+	}
+
+	public Lancamento getLancamento() {
+		return lancamento;
+	}
+
+	public void setLancamento(Lancamento lancamento) {
+		this.lancamento = lancamento;
+	}
+}
